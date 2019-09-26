9ac0a81b6ab2e85e835ff44ae58b49c4e80ed6b6
Guoyingxu <bxhgyx@163.com>
2019-09-26 14:57:28 +0800

    order

--- repository/QuestionRepository.ts
+++ repository/QuestionRepository.ts
@@ -6,13 +6,13 @@ import {Question} from '../entity/question/qusetion'
 export class QuestionRepository extends Repository<Question>{
   getIdsByRand(bankid,size){
     if(size!='0' && size !=0){
-      return getConnection().query(`select id from question where bankid=${bankid} order by RAND() limit ${size}`)
+      return getConnection().query(`select id from question where bankid=${bankid} order by RAND() limit ${size}  `)
     }else{
-      return getConnection().query(`select id from question`)
+      return getConnection().query(`select id from question order by id desc`)
     }
   }
   getallgaokao( ){
-    return getConnection().query(`select * from question where bankid in (2,3)`)
+    return getConnection().query(`select * from question where bankid in (2,3) order by id desc`)
   }
   // getQuestionListByBank(bankid,size){
   //   return this.createQueryBuilder('question')
